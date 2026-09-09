b8387e601a6f72058a2657d811ad28cfbe3793ed
fortisienergytech-jpg <fortisi.energy.tech@gmail.com>
2026-09-09 18:15:35 -0300

    Add files via upload

--- patio-conecta/sw.js
+++ patio-conecta/sw.js
@@ -1,20 +1,38 @@
-const CACHE='patio-conecta-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest',
-'./icons/icon-16.png','./icons/icon-32.png','./icons/icon-180.png',
-'./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+const CACHE_NAME = 'patio-conecta-final-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-32.png',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-    .then(()=>self.clients.claim())
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.origin===self.location.origin){
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-  }
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
+  );
 });
