b9e6924aa402abc88e925dc62337bb5f0284a1f8
fortisienergytech-jpg <fortisi.energy.tech@gmail.com>
2026-09-10 00:49:47 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='patio-conecta-ios-aware-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-32.png','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin!==self.location.origin)return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
